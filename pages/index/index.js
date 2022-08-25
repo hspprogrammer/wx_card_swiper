c83bd8e680cf35ff8938a1f50d313e1a36496759
hanshanpeng <hanshanpeng@jingrizf.com>
2022-08-25 19:39:09 +0800

    删除多余代码

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,11 +3,6 @@ Page({
       cardList:[1,2,3,4,5,6,7,8,9,10],
       nextPageData:[11,12,13,14,15,16,17,18,19,20]
   },
-  nextCard(e){
-      const {direction} = e.currentTarget.dataset;
-      const cardSwiper = this.selectComponent('#heartCardSwiper')
-      cardSwiper.nextCard(direction)
-  },
   cardChange(e){
       const {direction,index} = e.detail;
       if(index>this.data.cardList.length-3){
